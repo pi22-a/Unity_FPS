e1defab567333ac7485a1860cbf713e0613986a6
Park Jeong Hoon <pi22a555@naver.com>
2023-07-12 17:24:20 +0900

    0712

---
+++ Assets/Scripts/PatrolNChase/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace PatrolNChase
+{
+    public class Enemy : MonoBehaviour
+    {
+        NavMeshAgent agent;
+        public int targetIndex;
+        // Start is called before the first frame update
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 target = PathManager.instance.points[targetIndex].position;
+
+            agent.SetDestination(target);
+
+            // 만약 목적지에 도착했다면(두지점의 거리가 0.1M이하라면)
+            target.y = transform.position.y;
+            float dist = Vector3.Distance(transform.position, target);
+            if (dist <= 0.1f)
+            {
+                // 인덱스를 1증가시키고싶다.
+                targetIndex++;
+                // 만약 인덱스가 points배열의 크기이상이되면 0으로 초기화 하고싶다.
+                if (targetIndex >= PathManager.instance.points.Length)
+                {
+                    targetIndex = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PatrolNChase/PathManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PatrolNChase
+{
+    public class PathManager : MonoBehaviour
+    {
+        public static PathManager instance;
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        public Transform[] points;
+        // Start is called before the first frame update
+        void Start()
+        {
+            points = new Transform[transform.childCount];
+            // 태어날 때 points배열에 값을 채우고싶다.
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                points[i] = transform.GetChild(i);
+            }
+
+            //Transform[] temp = GetComponentsInChildren<Transform>();
+            //List<Transform> list = new List<Transform>(temp);
+            //// 만약 목록에 내 transform이 있다면
+            //if (list.Contains(transform))
+            //{
+            //    // 제외하고싶다.
+            //    list.Remove(transform);
+            //}
+            //points = list.ToArray();
+
+
+
+            //points = new Transform[temp.Length - 1];
+            //for (int i = 0, j = 0; i < temp.Length; i++)
+            //{
+            //    if (temp[i] == transform)
+            //        continue;
+
+            //    points[j] = temp[i];
+            //    j++;
+
+            //}
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
